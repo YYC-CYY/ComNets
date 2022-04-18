dc2a7331181f657ec375ad917a37a56ac5873771
YYC-CYY <1170742179@qq.com>
2022-04-18 16:44:07 +0800

    重新修改了三角定位法，分类讨论了圆与圆之间的相对位置，优化了数据结构和类型定义

--- IndoorPositioning/posmethod.cpp
+++ IndoorPositioning/posmethod.cpp
@@ -137,21 +137,21 @@ double dis(Point x, Point y){
     return sqrt((x.first - y.first)*(x.first - y.first) +(x.second - y.second)*(x.second - y.second));
 }
 
-int main(){
-    Node* node = new Node[3];
-    // node[0] = new _Node(); node[0]->r = 4; node[0]->p.first = 0; node[0]->p.second = 0;
-    // node[1] = new _Node(); node[1]->r = 4; node[1]->p.first = 4; node[1]->p.second = 0;
-    // node[2] = new _Node(); node[2]->r = 4; node[2]->p.first = 2; node[2]->p.second = 2;
-    node[0] = new _Node(); node[0]->r = 6; node[0]->p.first = 0; node[0]->p.second = 0;
-    node[1] = new _Node(); node[1]->r = 6; node[1]->p.first = 6; node[1]->p.second = 0;
-    node[2] = new _Node(); node[2]->r = 0.5; node[2]->p.first = 0; node[2]->p.second = 4;
+// int main(){
+//     Node* node = new Node[3];
+//     // node[0] = new _Node(); node[0]->r = 4; node[0]->p.first = 0; node[0]->p.second = 0;
+//     // node[1] = new _Node(); node[1]->r = 4; node[1]->p.first = 4; node[1]->p.second = 0;
+//     // node[2] = new _Node(); node[2]->r = 4; node[2]->p.first = 2; node[2]->p.second = 2;
+//     node[0] = new _Node(); node[0]->r = 6; node[0]->p.first = 0; node[0]->p.second = 0;
+//     node[1] = new _Node(); node[1]->r = 6; node[1]->p.first = 6; node[1]->p.second = 0;
+//     node[2] = new _Node(); node[2]->r = 0.5; node[2]->p.first = 0; node[2]->p.second = 4;
 
-    Point p = triangulation(node);
-    cout << "定位: " ;
-    cout << "(" << p.first << ", " << p.second << ")" << endl;
-    delete node[0];
-    delete node[1];
-    delete node[2];
-    delete []node;
-    return 0;
-}
+//     Point p = triangulation(node);
+//     cout << "定位: " ;
+//     cout << "(" << p.first << ", " << p.second << ")" << endl;
+//     delete node[0];
+//     delete node[1];
+//     delete node[2];
+//     delete []node;
+//     return 0;
+// }
